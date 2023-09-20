708f2b9369f9b158f47a7eaa00b82f1ee4cc4b8b
blakehonda99 <51813721+blakehonda99@users.noreply.github.com>
2023-09-20 18:38:23 +1200

    Update [[path]].ts

--- functions/raw/[[path]].ts
+++ functions/raw/[[path]].ts
@@ -7,10 +7,24 @@ export async function onRequestGet(context) {
   const obj = await bucket.get(path);
   if (obj === null) return notFound();
 
-  const headers = new Headers();
-  obj.writeHttpMetadata(headers);
-  if (path.startsWith("_$flaredrive$/thumbnails/"))
-    headers.set("Cache-Control", "max-age=31536000");
+  const url = new URL(context.request.url);
 
-  return new Response(obj.body, { headers });
+  // Check if the URL path ends with "/download"
+  if (url.pathname.endsWith("/download")) {
+    const headers = new Headers();
+    headers.set("Content-Disposition", `attachment; filename="${path.split('/').pop()}"`);
+    
+    // Optionally, set other headers like Content-Type if needed
+    // headers.set("Content-Type", "application/octet-stream");
+
+    return new Response(obj.body, { headers });
+  } else {
+    // Regular view behavior with appropriate headers
+    const headers = new Headers();
+    obj.writeHttpMetadata(headers);
+    if (path.startsWith("_$flaredrive$/thumbnails/"))
+      headers.set("Cache-Control", "max-age=31536000");
+
+    return new Response(obj.body, { headers });
+  }
 }
